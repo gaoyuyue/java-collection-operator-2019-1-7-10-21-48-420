8152830d67e01e70fecae0043c821fe67367d194
高语越 <gaoyuyue@outlook.com>
2019-05-10 21:28:25 +0800

    add test should return list by Interval

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,11 +2,19 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        if (left > right) {
+            List<Integer> result = IntStream.rangeClosed(right, left).boxed().collect(Collectors.toList());
+            Collections.reverse(result);
+            return result;
+        }
+        return IntStream.rangeClosed(left, right).boxed().collect(Collectors.toList());
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
